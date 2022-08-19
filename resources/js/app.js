8fc28c41acb65a911f355318098b3a1d384b3d45
Sanaullah Kiani <kianisanaullah@gmail.com>
2022-08-19 23:30:04 +0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,27 @@
 import './bootstrap';
 
-
 import '../sass/app.scss'
-import lame from 'lamejs';
-
 
 import {createApp} from 'vue'
-import App from './Components/App.vue'
+
+import lame from 'lamejs';
 import AudioRecorder from 'vue-audio-recorder'
 
 
+import App from './Components/App.vue'
 import ExampleComponent1 from './Components/ExampleComponent1.vue'
 import ExampleComponent2 from './Components/ExampleComponent2.vue'
 // import AudioComponent from './Components/AudioRecordComponent.vue'
-
-createApp(AudioRecorder).mount()
-createApp(App).mount("#app")
-createApp(ExampleComponent1).mount("#root-component-1")
-createApp(ExampleComponent2).mount("#root-component-2")
+createApp({
+    components: {
+        App,
+        ExampleComponent1,
+        ExampleComponent2
+    }
+}).use(lamejs,AudioRecorder).mount('#app')
+// createApp(AudioRecorder).mount()
+// createApp(App).mount("#app")
+// createApp(ExampleComponent1).mount("#root-component-1")
+// createApp(ExampleComponent2).mount("#root-component-2")
 // createApp(AudioComponent).mount("#audio-component")
 
