6482f53abe9736d1ce3d578ebca8c84ed773d3a8
Sanaullah Kiani <kianisanaullah@gmail.com>
2022-08-19 22:51:55 +0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,14 +5,14 @@ import '../sass/app.scss'
 
 import {createApp} from 'vue'
 import App from './Components/App.vue'
-import AudioRecorder from 'vue-audio-recorder'
+// import AudioRecorder from 'vue-audio-recorder'
 
 
 import ExampleComponent1 from './Components/ExampleComponent1.vue'
 import ExampleComponent2 from './Components/ExampleComponent2.vue'
 // import AudioComponent from './Components/AudioRecordComponent.vue'
 
-createApp(AudioRecorder).mount()
+// createApp(AudioRecorder).mount()
 createApp(App).mount("#app")
 createApp(ExampleComponent1).mount("#root-component-1")
 createApp(ExampleComponent2).mount("#root-component-2")
